9ed419ffa2f79e47fd2218005b94db42ffc0ac68
prahal <vprahaladhan@gmail.com>
2021-03-22 17:28:51 +1100

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string) {
+    return string[0].toUpperCase() + string.slice(1);
+  }
+
+  static sanitize(string) {
+    // removes any non alpha-numeric characters except dash and single quotes (apostrophes)
+    return string.replace(/[^A-Za-z0-9-' ]+/g, '');
+  }
+
+  static titleize(string) {
+    const articles = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from'];
+
+    return string.split(' ').map((word, index) => (articles.includes(word) && index !== 0) ? word : this.capitalize(word)).join(' ');
+  }
 }
